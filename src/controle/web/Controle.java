b60c8706e311026f0ddbea234ccc39da46b3cdfb
Erika Silva <77064376+erikassilva@users.noreply.github.com>
2022-06-22 15:03:48 -0300

    Add files via upload

--- src/controle/web/Controle.java
+++ src/controle/web/Controle.java
@@ -1,7 +1,54 @@
 package controle.web;
 
+import dominio.EntidadeDominio;
 
-public class Controle {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-	
+@WebServlet(name = "Servlet", value = "/Servlet")
+public class Controle extends HttpServlet {
+
+    private Map<String, IViewHelper> vhs = new HashMap<String, IViewHelper>();
+    private Map<String, ICommand> cmds = new HashMap<String, ICommand>();
+
+    public Controle() {
+
+        vhs.put("/CadastroFornecedores/SalvarFornecedor", new FornecedorVH());
+
+        cmds.put("Salvar", new SalvarCommand());
+        cmds.put("Alterar", new AlterarCommand());
+        cmds.put("Consultar", new ConsultarCommand());
+        cmds.put("Excluir", new ExcluirCommand());
+
+    }
+
+    private void processRequest(HttpServletRequest request,
+                                HttpServletResponse response) throws IOException {
+
+        String uri = request.getRequestURI();
+        String operacao = request.getParameter("operacao");
+
+        IViewHelper vh = vhs.get(uri);
+
+        EntidadeDominio entidade = vh.getEntidade(request);
+
+        ICommand cmd = cmds.get(operacao);
+
+        Object msg = cmd.executar(entidade);
+
+        vh.setEntidade(response, request, msg);
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
 }
--- src/controle/web/FornecedorVH.java
+++ src/controle/web/FornecedorVH.java
@@ -1,142 +1,73 @@
 package controle.web;
 
+import dominio.*;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import dominio.Cidade;
-import dominio.Cnae;
-import dominio.ContaBancaria;
-import dominio.Contato;
-import dominio.Endereco;
-import dominio.EntidadeDominio;
-import dominio.Fornecedor;
-import dominio.Pais;
-import dominio.Telefone;
-import dominio.TipoEndereco;
-import dominio.TipoLogradouro;
-import dominio.TipoTelefone;
-import dominio.Uf;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class FornecedorVH implements IViewHelper {
+
 
     @Override
     public EntidadeDominio getEntidade(HttpServletRequest request) {
 
         //Dados Pais
 
-        Pais pais = new Pais();
-
         String paisDescricao = request.getParameter("txtDescricaoPais");
 
+        Pais pais = new Pais();
         pais.setDescricao(paisDescricao);
 
         //Dados Estado(UF)
 
-        Uf estado = new Uf();
-
         String estadoDescricao = request.getParameter("txtEstadoDescricao");
         String estadoSigla = request.getParameter("txtEstadoSigla");
 
+        Uf estado = new Uf("","",pais);
         estado.setDescricao(estadoDescricao);
         estado.setSigla(estadoSigla);
 
         //Dados Cidade
 
-
-        Cidade cidade = new Cidade();
-
         String cidadeDescricao = request.getParameter("txtCidadeDescricao");
 
+        Cidade cidade = new Cidade("",estado);
         cidade.setDescricao(cidadeDescricao);
 
         //Dados do Tipo de Endereco
 
-        TipoEndereco tipoEndereco = new TipoEndereco();
-
         String tipoNome = request.getParameter("txtTipoNome");
         String tipoDescricao = request.getParameter("txtTipoDescricao");
 
+        TipoEndereco tipoEndereco = new TipoEndereco();
         tipoEndereco.setNome(tipoNome);
         tipoEndereco.setDescricao(tipoDescricao);
 
         //Dados do Tipo de Logradouro
 
-        TipoLogradouro tipoLogradouro= new TipoLogradouro();
-
         String logTipoNome = request.getParameter("txtTipoDeLogradouroNome");
         String logTipoDescricao = request.getParameter("txtTipoDeLograrouroDescricao");
 
+        TipoLogradouro tipoLogradouro = new TipoLogradouro();
         tipoLogradouro.setNome(logTipoNome);
         tipoLogradouro.setDescricao(logTipoDescricao);
 
         // Dados de Endereco.
-
-        Endereco endereco = new Endereco();
 
         String endCEP = request.getParameter("txtEndCEP").replace(".", "").replace("/", "").replace("-", "");
         String endNumero = request.getParameter("txtEndNumero");
         String endBairro = request.getParameter("txtEndBairro");
         String endComplemento = request.getParameter("txtEndComplemento");
 
+        Endereco endereco = new Endereco("","","","",cidade,tipoLogradouro,tipoEndereco);
         endereco.setBairro(endBairro);
         endereco.setNumero(endNumero);
         endereco.setCep(endCEP);
         endereco.setComplemento(endComplemento);
 
-        //Dados do tipo de Telefone
-
-        TipoTelefone tipoTelefone = new TipoTelefone();
-
-        String tipoDescFornecedor = request.getParameter("txtTipoTelDescFornecedor");
-
-        tipoTelefone.setDescricao(tipoDescFornecedor);
-
-        //Dados do Telefone
-
-        Telefone telefone = new Telefone();
-
-        String dddTelefone = request.getParameter("txtDDDTelefoneFornecedor");
-        String numeroTelefone = request.getParameter("txtNumeroFornecedor");
-        String ddiTelefone= request.getParameter("txtDDIFornecedor");
-
-        telefone.setDdd(dddTelefone);
-        telefone.setNumero(numeroTelefone);
-        telefone.setDdi(ddiTelefone);
-
-        //Dados Conta Bancaria
-
-        ContaBancaria contaBancaria = new ContaBancaria();
-
-        String bancoFornecedor = request.getParameter("txtBancoFornecedor");
-        String agenciaFornecedor = request.getParameter("txtAgenciaFornecedor");
-        String contaFornecedor = request.getParameter("txtContaFornecedor");
-
-        contaBancaria.setBanco(bancoFornecedor);
-        contaBancaria.setAgencia(agenciaFornecedor);
-        contaBancaria.setConta(contaFornecedor);
-
-        //Dados Cnae
-
-        Cnae cnae = new Cnae();
-
-        String cnaeDescricao = request.getParameter("txtCnaeDescricaoFornecedor");
-
-        cnae.setNumero(cnaeDescricao);
-
-        //Dados do Contato
-
-        Contato contato = new Contato();
-
-        String emailFor = request.getParameter("txtEmailContatoFornecedor");
-
-        contato.setEmail(emailFor);
-
         //Dados de Fornecedor
-
-        Fornecedor fornecedor = new Fornecedor();
 
         String emailFornecedor = request.getParameter("txtEmailFornecedor");
         String cnpjFornecedor = request.getParameter("txtCnpjFornecedor");
@@ -147,6 +78,8 @@ public class FornecedorVH implements IViewHelper {
         String statusFornecedor = request.getParameter("txtStatusFornecedor");
         String tipForFornecedor = request.getParameter("txttipoFornecimentoFornecedor");
 
+
+        Fornecedor fornecedor = new Fornecedor("","","","","","","","",endereco);
         fornecedor.setEmail(emailFornecedor);
         fornecedor.setCnpj(cnpjFornecedor);
         fornecedor.setInscricaoEstadual(incrEstFornecedor);
@@ -159,6 +92,7 @@ public class FornecedorVH implements IViewHelper {
         return fornecedor;
     }
 
+    @Override
     public void setEntidade(HttpServletResponse response, HttpServletRequest request, Object msg) {
 
         PrintWriter out;
@@ -173,6 +107,8 @@ public class FornecedorVH implements IViewHelper {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+
     }
+
 }
 
